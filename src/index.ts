9b3d4fbb990eb6bbefa1cb7c33202172184e0bca
Diaa <deiaa_x@yahoo.com>
2020-11-28 21:29:09 +0200

    add third paragraph to home page

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,11 @@ var server = http.createServer(function (req: any, res: any) {   //create web se
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     // set response content    
-    res.write('<html><body><p>This is home Page.</p><p>second paragraph</p></body></html>');
+    res.write(`<html><body>
+      < p > This is home Page.< /p>
+      <p>second paragraph</p >
+      <p>third paragraph</p >
+      </body></html >`);
     res.end();
 
   }
